26f9968c65d6e7d9cc86dff5b4d4bac082a5a219
0xVorian <33686588+0xVorian@users.noreply.github.com>
2024-04-26 11:40:35 +0200

    added mainnet address for price fetching using defillama

--- src/global.config.js
+++ src/global.config.js
@@ -2,31 +2,13 @@ const tokens = {
   WBNB: {
     decimals: 18,
     address: '0x4200000000000000000000000000000000000006',
+    mainnetAddress: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
     dustAmount: 0.001
-  },
-  BTCB: {
-    decimals: 18,
-    address: '0x7c6b91d9be155a6db01f749217d76ff02a7227f2',
-    dustAmount: 0.000001
-  },
-  DAI: {
-    decimals: 18,
-    address: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
-    dustAmount: 0.01
-  },
-  ETH: {
-    decimals: 18,
-    address: '0xe7798f023fc62146e8aa1b36da45fb70855a77ea',
-    dustAmount: 0.00001
   },
   USDT: {
     decimals: 18,
     address: '0x9e5aac1ba1a2e6aed6b32689dfcf62a509ca96f3',
-    dustAmount: 0.01
-  },
-  FDUSD: {
-    decimals: 18,
-    address: '0x50c5725949a6f0c72e6c4a641f24049a917db0cb',
+    mainnetAddress: '0x55d398326f99059fF775485246999027B3197955',
     dustAmount: 0.01
   },
 };
@@ -34,85 +16,14 @@ const tokens = {
 const watchedPairs = {
   WBNB: [
     {
-      quote: 'BTCB',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'USDC',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'ETH',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
       quote: 'USDT',
       pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'FDUSD',
-      pivots: ['USDT'],
-      exportToInternalDashboard: true
-    },
-  ],
-  BTCB: [
-    {
-      quote: 'USDC',
-      pivots: ['USDT'],
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'ETH',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'USDT',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'FDUSD',
-      pivots: ['USDT'],
-      exportToInternalDashboard: true
-    },
-  ],
-  USDC: [
-    {
-      quote: 'ETH',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'USDT',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'FDUSD',
-      pivots: ['USDT'],
-      exportToInternalDashboard: true
-    },
-  ],
-  ETH: [
-    {
-      quote: 'USDT',
-      pivots: undefined,
-      exportToInternalDashboard: true
-    },
-    {
-      quote: 'FDUSD',
-      pivots: ['USDT'],
       exportToInternalDashboard: true
     },
   ],
   USDT: [
     {
-      quote: 'FDUSD',
+      quote: 'WBNB',
       pivots: undefined,
       exportToInternalDashboard: true
     },
